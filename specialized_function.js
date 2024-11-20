66ebdccd73d8d23ec9d93fa58232b1b5bcf2037b
Guilherme Valentim <valentim.guilherme@gmail.com>
2024-11-20 20:09:29 -0300

    fix on specialized functions

--- specialized_function.js
+++ specialized_function.js
@@ -1,7 +1,7 @@
-const factormultiply = (factor, number) => factor * number;
+const factormultiply = (factor) => (number) => factor * number;
 
-const specializedDoubleFunction = (number) => factormultiply(2, number);
-const specializedTripleFunction = (number) => factormultiply(3, number);
+const specializedDoubleFunction = factormultiply(2);
+const specializedTripleFunction = factormultiply(3);
 
 console.log("Double of 15, is: " + specializedDoubleFunction(15));
 console.log("Triple of 15, is: " + specializedTripleFunction(15));
