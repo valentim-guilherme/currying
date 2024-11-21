66bea860733e72ef0c8983fb90f2f883680e93c1
Guilherme Valentim <valentim.guilherme@gmail.com>
2024-11-20 21:03:53 -0300

    adding a second function on the generic curry function

--- generic_curry_function.js
+++ generic_curry_function.js
@@ -1,7 +1,3 @@
-function addThreeNumbers(a, b, c) {
-  return a + b + c;
-}
-
 function curry(fn) {
   return function curried(...params) {
     if (params.length >= fn.length) {
@@ -15,11 +11,28 @@ function curry(fn) {
   };
 }
 
-const curriedAdd = curry(addThreeNumbers);
+function addThreeNumbers(a, b, c) {
+  return a + b + c;
+}
 
+function calculateTotal(price, taxRate, discount) {
+  return price + price * (taxRate / 100) - discount;
+}
+
+const curriedAdd = curry(addThreeNumbers);
+const curriedCalculateTotal = curry(calculateTotal);
+
+//FIRST CURRIED FUNCTION START
 const complete = curriedAdd(1)(2)(3); // Output: 6
 const missingOne = curriedAdd(1, 2); // [Function (anonymous)], bc it will miss the last parameter
 
 console.log(complete);
 console.log(missingOne); // [Function (anonymous)], bc it will miss the last parameter c
 console.log(missingOne(3)); // Missing one with the last parameter
+//FIRST CURRIED FUNCTION END
+
+//SECOND CURRIED FUNCTION START
+const apply10PercentTax = (price) => curriedCalculateTotal(price, 10);
+console.log(apply10PercentTax(100)(20));
+
+//FIRST CURRIED FUNCTION END
